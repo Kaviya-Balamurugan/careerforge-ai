a57e164f2f2ba1b7f7e2c191a551819ff4d2b6aa
Kaviya-Balamurugan <kaviyabalamurugan03@gmail.com>
2026-06-13 10:14:36 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "CareerForge AI Backend Running"}
+
+@app.post("/upload-resume")
+async def upload_resume(file: UploadFile = File(...)):
+    
+    file_path = f"uploads/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "Resume uploaded successfully",
+        "filename": file.filename
+    }
